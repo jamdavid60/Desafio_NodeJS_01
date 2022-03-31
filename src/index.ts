9966430ae2c32d7c8211de2829fbae7a88a30904
jamdavid60 <jamdavid60@gmail.com>
2022-03-31 10:05:06 -0300

    desafio concluido

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
-/**
- * Required External Modules
- */
-
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
 dotenv.config();
-
-/**
- * App Variables
- */
 
 if (!process.env.PORT) {
 	process.exit(1);
@@ -21,21 +13,13 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-/**
- *  App Configuration
- */
-
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-/**
- * Server Activation
- */
-
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
-  // Escreva aqui a sua msg para o mundo
-  
+	console.log("Olá mundo, Bora quebrar tudo!");
+	
 });
